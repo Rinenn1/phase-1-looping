1bc700d88e5929ad2380f9481dcb62ff26a5bc6e
Isaac <bennisaac24@gmail.com>
2024-12-13 22:37:15 +0300

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+    let printMsg = [];
+    let i = 0;
+    for (i = 0; i < names.length; i++){
+        let msg = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        printMsg.push(msg);
+    }
+    return printMsg;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise');
+
+
+function countDown(){
+    let num = 10;
+    while (num >= 0){
+        console.log(num--);
+    }
+}
+
+countDown();
